de8f78cc2d9dd697faca2a0a535b5f3a6ad395d2
Rodrigo Flores Mayta <rodrixfloresm@gmail.com>
2024-07-22 15:36:28 -0500

    Flores - Parte 2 - Base de Datos

--- ExamenFinalLP3/miapp/models.py
+++ ExamenFinalLP3/miapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Flores_Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.TextField()
+    sexo = models.CharField(max_length=100)
+    # auto_now_add me permitirá registrar
+    # la fecha cuando cree el registro
+    fecha_registro = models.DateTimeField(auto_now_add=True)
